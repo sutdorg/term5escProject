d3e44df0b155be25bcb8ac9816911268761ad55c
Shiinx <z.shiinx@gmail.com>
2020-04-05 05:47:36 +0800

    index starter

--- Alternative/IntegratedBackend/index.js
+++ Alternative/IntegratedBackend/index.js
@@ -1,3 +1,16 @@
 const sdk = require('sdk.js');
 const backend = require('be.js');
-const database = require('db.js');
+const database = require('temp.js');
+
+
+// TODO: create sdk config & db config
+let configSDK = {};
+let configDB = {};
+
+sdk.start(configSDK).then(res => {
+    database.start(configDB).then(res => {
+       backend.start().then(res => {
+           console.log("all started");
+       });
+    });
+});
